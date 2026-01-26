42b504c7212a267a68b1bcdfdf411aa5fba2d3a3
WesleyHui <72075816+Wesley436@users.noreply.github.com>
2026-01-26 10:58:53 -0500

    connect to firebase

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+import { firebaseDB } from './config/Firebase.js';
+import { getDocs, collection } from 'firebase/firestore';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const express = require('express');
 const app = express();
 const port = 8080;
 
 // Define a route for GET requests to the root URL
-app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+app.get('/', async (req, res) => {
+  const querySnapshot = await getDocs(collection(firebaseDB, "test"));
+      querySnapshot.forEach((doc) => {
+      res.send(`${doc.id} => ${doc.data().field}`);
+    }
+  );
+
+  // res.send('Hello World from Express!');
 });
 
 // Start the server
